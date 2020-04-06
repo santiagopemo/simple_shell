9e62b0e7a609cf0a7dfde5b777ad164f67ca3dad
fabianmosquera <rfmq9530@hotmai.com>
2020-04-06 14:41:43 -0500

    Library

---
+++ shell.c
@@ -0,0 +1,13 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*add the struct */
+
+/*add the protoytypes */
+
+#endif /* SHELL */
