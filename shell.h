0f751213471e1070b4627f86b252172ed46cebd5
fabianmosquera <rfmq9530@hotmai.com>
2020-04-07 16:20:40 -0500

    library with 3 prototypes

--- shell.h
+++ shell.h
@@ -6,8 +6,14 @@
 #include <unistd.h>
 #include <string.h>
 
+extern char **environ;
+
 /*add the struct */
 
 /*add the protoytypes */
+int _strcmp(char *s1, char *s2);
+void prompt(void);
+char *_env(char *env);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* SHELL */
