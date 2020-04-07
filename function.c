dc4643c12722d7f5406e7af16cfb6e9736b94aa9
fabianmosquera <rfmq9530@hotmai.com>
2020-04-07 16:21:18 -0500

    with 2 more functions

--- function.c
+++ function.c
@@ -11,3 +11,51 @@ void prompt(void)
 		write(1, "$ ", 2);
 	}
 }
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: Difference of first nonmatching character
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
+/**
+ * _strstr - locates sub string
+ * @haystack: input one
+ * @needle: input two
+ * Return: sub string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
