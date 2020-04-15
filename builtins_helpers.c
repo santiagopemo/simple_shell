2ad22ea5204cbdfd267bcb6b7c32298a62fe7457
santiagopemo <santiagopemo@gmail.com>
2020-04-15 18:21:09 +0000

    add builtins_helpers file

---
+++ builtins_helpers.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+int _atoi(char *s)
+{
+	register int i = 0;
+	unsigned long num = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + s[i] - '0';
+		else
+			return (-1);
+		i++;
+	}
+	if (num > INT_MAX)
+	{
+		return (-1);
+	}
+	return (num % 256);
+}
+/**
+ * _uitoa - converts a unsigned to an string
+ * @count: unsigned
+ *
+ * Return: string whit the number
+ */
+char *_uitoa(unsigned int count)
+{
+	char *numstr;
+	unsigned int tmp, digits;
+
+	tmp = count;
+	for (digits = 0; tmp != 0; digits++)
+		tmp /= 10;
+	numstr = malloc(sizeof(char) * (digits + 1));
+	if (numstr == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	numstr[digits] = '\0';
+	for (--digits; count; --digits)
+	{
+		numstr[digits] = (count % 10) + '0';
+		count /= 10;
+	}
+	return (numstr);
+}
